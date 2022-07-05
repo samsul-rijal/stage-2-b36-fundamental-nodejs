41d49996df7892ad3f7a90ac23f2d599c87a4411
Your Name <samsulr99@gmail.com>
2022-07-05 10:03:45 +0700

    update

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const port = 5000
 
 //allow this app to receive incoming json request
 //Create app.use for express.json here
+app.use(express.json())
 
 let todos = [
     {
@@ -21,18 +22,64 @@ let todos = [
 
 //GET list route: simply send arr of obj todos on your user screen
 // Create method GET here
+app.get("/todos", (req, res) => {
+    res.send({
+       data: todos
+    })
+})
+
 
 //GET detail route: send the todo obj, by received id request params
 // Create method GET by received id here
+app.get("/todo/:id", (req, res) => {
+
+    let id  = req.params.id
+
+    let index = id - 1
+    res.send(todos[index])
+})
+
 
 //POST route: receive json body request, from user input, then push to todos array
 // Create method POST here
+app.post("/todo",(req, res) => {
+    todos = [...todos, req.body]
+
+    res.send({
+        todos
+    })
+})
+
 
 //PATCH route: receive json body request, from user input, then push to todos array
 //by object id
 // Create method PATCH here
+app.patch("/todo/:id", (req, res) => {
+    const {id} = req.params
+
+    let index = id - 1
+
+    todos[index] = {...todos[index], ...req.body}
+
+    res.send({
+        data: todos[index]
+    })
+})
+
 
 //DELETE route: delete the todo obj, by received id request params
 // Create method DELETE here
+app.delete("/todo/:id", (req, res) => {
+
+    const {id} = req.params
+
+    todos = todos.filter((item) => item.id != id)
+
+    res.send({
+        data: todos
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Listening on port ${port}!`))
